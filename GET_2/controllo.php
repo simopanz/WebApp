b0b426550e352f311827200e33baab75e49bb4c4
simopanz <simonepanzeri.sp@gmail.com>
2025-10-20 13:43:54 +0200

    Add

--- GET_2/controllo.php
+++ GET_2/controllo.php
@@ -1,12 +1,31 @@
-<?php
-$utenti = array("simone"=>"123", "simon"=>"456", "simo"=>"789");
-?>
-
 <html>
     <head></head>
     <body>
         <h1>Controllo credenziali</h1>
         <?php
+            /**
+             * Leggere un file json:
+             * - $path: utenti.json
+             * - file_exists($path) --> TRUE se esiste il file, FALSE altrimenti
+             * Stampare il contenuto a video
+             */
+            $path = "utenti.json";
+            if (!file_exists($path))
+                die("Errore, il file non esiste");
+            else {
+                // leggere il contenuto
+                $json = file_get_contents($path);
+                //var_dump($json);
+                $dati = json_decode($json, true); // true --> array
+                //var_dump($dati);
+                $utenti = array();
+                foreach ($dati as $valore) {
+                    foreach ($valore as $k=>$v) {
+                        $utenti[$k] = $v;
+                        echo("$k: $v</br>");
+                    }
+                }
+            }
             $k = $_GET["login"];
             $v = $utenti[$k];
             if ($v === $_GET["psw"]) {
