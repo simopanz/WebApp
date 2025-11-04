065f563af8ae061a2ddd62198d22bff790a9065f
simopanz <simonepanzeri.sp@gmail.com>
2025-11-04 23:17:52 +0100

    Add

--- GET_2/salvaUtente.php
+++ GET_2/salvaUtente.php
@@ -3,26 +3,26 @@
     if (!file_exists($nomeFile)) {
         die("xdffdsfsdf");
     } else {
-    $json = file_get_contents($nomeFile);
-    $array = json_decode($json, true);
-    
-    $nuovoUtente = [
-        "login" => "SIMO",
-        "psw" => "PANZ"
-    ];
+        $json = file_get_contents($nomeFile);
+        $array = json_decode($json, true);
+        
+        $nuovoUtente = [
+            "login" => "SIMO",
+            "psw" => "PANZ"
+        ];
 
-    $array[] = $nuovoUtente;
+        $array[] = $nuovoUtente;
 
-    foreach($array as $utente){
-        echo ("<p>");
-        foreach($utente as $k=>$v){
-            echo ("$k: $v </br>");
+        foreach($array as $utente){
+            echo ("<p>");
+            foreach($utente as $k=>$v){
+                echo ("$k: $v </br>");
+            }
+            echo ("</p>");    
         }
-        echo ("</p>");    
-    }
 
-    $json = json_encode($array, JSON_PRETTY_PRINT);
-    echo($json);
-    file_put_contents($nomeFile, $json);
+        $json = json_encode($array, JSON_PRETTY_PRINT);
+        echo($json);
+        file_put_contents($nomeFile, $json);
     }
 ?>
