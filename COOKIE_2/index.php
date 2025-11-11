5e41261e1bddd1fd4cd923a7be7267e1f5f1009d
simopanz <simonepanzeri.sp@gmail.com>
2025-11-12 00:27:17 +0100

    Add

--- COOKIE_2/index.php
+++ COOKIE_2/index.php
@@ -1,3 +1,20 @@
+<?php
+if (isset($_POST['nome'], $_POST['cognome'], $_POST['pers_font'], $_POST['pers_color'], $_POST['pers_background'])) {
+    $nomeP = $_POST['nome'];
+    $cognomeP = $_POST['cognome'];
+    $persP = $_POST['pers_font'] .'|'. $_POST['pers_color'] .'|'. $_POST['pers_background'];
+    setcookie('nome', $nomeP, time()+3600, '/');
+    setcookie('cognome', $cognomeP, time()+3600, '/');
+    setcookie('pers', $persP, time()+3600, '/');
+}
+if (isset($_COOKIE['pers'])) {
+    $lista = explode('|', $_COOKIE['pers']);
+    $font = if ($lista[0] === '');
+    $colore = $lista[1];
+    $bg = $lista[2];
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,19 +22,49 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>Esercizio COOKIE_2</title>
     <style>
-        <?php if (isset($_COOKIE['pers'])) $lista = explode('|', $_COOKIE['pers']); ?>
-        body {font-family="<?php $lista[0] ?>"; color:"<?php $lista[1] ?>"; background-color:"<?php $lista[2] ?>";}
+        body {font-family='<?php echo $font?>'; color: <?php echo $colore ?>; background-color: <?php echo $bg ?>;}
     </style>
 </head>
 <body>
     <?php
     if (isset($_COOKIE['nome'], $_COOKIE['cognome'], $_COOKIE['pers'])) {
-        $nome = $_COOKIE['nome'];
-        $cognome = $_COOKIE['cognome'];
-        $pers = $_COOKIE['pers'];
-        echo "<h1>Benvenuto $nome $cognome</h1>"; ?>
-        <!--
+        $nomeC = $_COOKIE['nome'];
+        $cognomeC = $_COOKIE['cognome'];
+        echo "<h1>Benvenuto $nomeC $cognomeC</h1>"; ?>
         <h3>Aggiorna i dati</h3>
+        <form action="index.php" method="post">
+            <div>
+                <label for="nome">Nome:</label>
+                <input type="text" name="nome" id="nome">
+            </div>
+            <div>
+                <label for="cognome">Cogome:</label>
+                <input type="text" name="cognome" id="cognome">
+            </div>
+            <div>
+                <label for="pers_font">Font:</label>
+                <select name="pers_font" id="pers_font">
+                    <option value="Arial">Arial</option>
+                    <option value="Verdana">Verdana</option>
+                    <option value="Times New Roman">Times New Roman</option>
+                    <option value="Courier New">Courier New</option>
+                    <option value="Georgia">Georgia</option>
+                </select>
+            </div>
+            <div>
+                <label for="pers_color">Colore font:</label>
+                <input type="color" name="pers_color" id="pers_color">
+            </div>
+            <div>
+                <label for="pers_background">Colore sfondo:</label>
+                <input type="color" name="pers_background" id="pers_background">
+            </div>
+            <div>
+                <input type="submit" name="sub" value="Send">
+            </div>
+        </form>
+    <?php } else { ?>
+        <h3>Inserisci i dati</h3>
         <form action="index.php" method="post">
             <div>
                 <label for="nome">Nome:</label>
@@ -29,7 +76,13 @@
             </div>
             <div>
                 <label for="pers_font">Font:</label>
-                <input type="text" name="pers_font" id="pers_font" required>
+                <select name="pers_font" id="pers_font" required>
+                    <option value="Arial">Arial</option>
+                    <option value="Verdana">Verdana</option>
+                    <option value="Times New Roman">Times New Roman</option>
+                    <option value="Courier New">Courier New</option>
+                    <option value="Georgia">Georgia</option>
+                </select>
             </div>
             <div>
                 <label for="pers_color">Colore font:</label>
@@ -43,40 +96,6 @@
                 <input type="submit" name="sub" value="Send">
             </div>
         </form>
-        -->
-    <?php } else { ?>
-        <form action="index.php?sub" method="post">
-            <div>
-                <label for="nome">Nome:</label>
-                <input type="text" name="nome" id="nome" required>
-            </div>
-            <div>
-                <label for="cognome">Cogome:</label>
-                <input type="text" name="cognome" id="cognome" required>
-            </div>
-            <div>
-                <label for="pers_font">Font:</label>
-                <input type="text" name="pers_font" id="pers_font" required>
-            </div>
-            <div>
-                <label for="pers_color">Colore font:</label>
-                <input type="color" name="pers_color" id="pers_color" required>
-            </div>
-            <div>
-                <label for="pers_background">Colore sfondo:</label>
-                <input type="color" name="pers_background" id="pers_background" required>
-            </div>
-            <div>
-                <input type="submit" name="sub" value="Send">
-            </div>
-        </form>
-    <?php
-        if (isset($_POST['nome'], $_POST['cognome'], $_POST['pers_font'], $_POST['pers_color'], $_POST['pers_background'])) {
-            $pers = $_POST['pers_font'] .'|'. $_POST['pers_color'] .'|'. $_POST['pers_background'];
-            setcookie('nome', $_POST['nome'], time()+3600, '/');
-            setcookie('cognome', $_POST['cognome'], time()+3600, '/');
-            setcookie('pers', $pers, time()+120, '/');
-        }
-    } ?>
+    <?php } ?>
 </body>
 </html>
